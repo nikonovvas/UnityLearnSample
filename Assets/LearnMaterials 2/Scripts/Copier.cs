4acdb7916105ae4c7b17fc8544af4c9c9d72ec80
Marina Kuklina <marina3hgf@gmail.com>
2022-05-06 14:22:41 +0700

    Copier 2 часть 5 работы

---
+++ Assets/LearnMaterials 2/Scripts/Copier.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Copier : Use
+{
+    [SerializeField]
+    private GameObject target;
+    private GameObject new_object;
+    [SerializeField]
+    [Min(1)]
+    private int amount;
+    [SerializeField]
+    private float step;
+    private Vector3 location = new Vector3(0, 0, 0);
+
+    [ContextMenu("Use")]
+    public void Use()
+    {
+        Copy();
+    }
+
+    public void Copy()
+    {
+        location = transform.position;
+        for (int i = 0; i < amount; i++)
+        {
+            location.x += step;
+            new_object = Instantiate(target, location, Quaternion.identity);
+        }
+    }
+
+    // Start is called before the first frame update
+
+}
